5065228ffbaa697f663651a302b64760d13f35a0
weishuping <weishp3@yonyou.com>
2018-05-23 11:13:23 +0800

    浮动元素测试

--- old/learnJS.js
+++ old/learnJS.js
@@ -346,24 +346,53 @@ EventTarget.prototype = {
 	}
 }
 //自定义事件
+// var Event = {
+// 	on: function(eventName, callback) {
+// 		if(!this.handlers) {
+// 			this.handlers = {};
+// 		}
+// 		if(!this.handlers[eventName]) {
+// 			this.handlers[eventName] = []
+// 		}
+// 		this.handlers[eventName].push(callback);
+// 	},
+// 	emit: function(eventName) {
+// 		if(this.handlers[eventName]) {
+// 			for(var i=0; i<this.handlers[arguments[0]].length; i++) {
+// 				this.handlers[arguments[0]][i](arguments[1]);
+// 			}
+// 		}
+// 	}	
+// }
 var Event = {
-	on: function(eventName, callback) {
-		if(!this.handlers) {
-			this.handlers = {};
+    // 通过on接口监听事件eventName
+    // 如果事件eventName被触发，则执行callback回调函数
+    on: function (eventName, callback) {
+        if(!this.handlers) {
+			// this.handlers = {};
+			Object.defineProperty(this, 'handlers', {
+				value: {},
+				enumerable: false,
+				configurable: true,
+				writable: true
+			});
 		}
 		if(!this.handlers[eventName]) {
 			this.handlers[eventName] = []
 		}
 		this.handlers[eventName].push(callback);
-	},
-	emit: function(eventName) {
+    },
+    // 触发事件 eventName
+    emit: function (eventName) {
+		//你的代码
 		if(this.handlers[eventName]) {
 			for(var i=0; i<this.handlers[arguments[0]].length; i++) {
 				this.handlers[arguments[0]][i](arguments[1]);
 			}
 		}
-	}	
-}
+    }
+};
+
 // 测试1
 Event.on('test', function (result) {
     console.log(result);
@@ -372,5 +401,21 @@ Event.on('test', function () {
     console.log('test');
 });
 Event.emit('test', 'hello world'); // 输出 'hello world' 和 'test'
+
+// 测试2
+var person1 = {};
+var person2 = {};
+Object.assign(person1, Event);
+Object.assign(person2, Event);
+person1.on('call1', function () {
+    console.log('person1');
+});
+person2.on('call2', function () {
+    console.log('person2');
+});
+person1.emit('call1'); // 输出 'person1'
+person1.emit('call2'); // 没有输出
+person2.emit('call1'); // 没有输出
+person2.emit('call2'); // 输出 'person2'
 
 
